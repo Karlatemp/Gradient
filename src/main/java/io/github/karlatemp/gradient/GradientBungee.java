00e72cc2f008c5ad3fe603eb296303ebb06c4aa0
Karlatemp <karlatemp@vip.qq.com>
2020-07-06 17:03:24 +0800

    Bungee

---
+++ src/main/java/io/github/karlatemp/gradient/GradientBungee.java
@@ -0,0 +1,14 @@
+/*
+ * Copyright (c) 2018-2020 Karlatemp. All rights reserved.
+ * @author Karlatemp <karlatemp@vip.qq.com> <https://github.com/Karlatemp>
+ * @create 2020/07/06 17:02:58
+ *
+ * gradient/gradient.main/GradientBungee.java
+ */
+
+package io.github.karlatemp.gradient;
+
+import net.md_5.bungee.api.plugin.Plugin;
+
+public class GradientBungee extends Plugin {
+}
